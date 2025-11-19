856ddd2fd62987a47e8f99845339535a1e74e07c
Feda7 <feda.a19390@gmail.com>
2025-11-19 20:52:08 +0300

    add

--- stores/user.js
+++ stores/user.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     userData: {}   // هنا تتجمع بيانات المستخدم كلها
+  }),
 
   actions: {
     setUserData(data) {
