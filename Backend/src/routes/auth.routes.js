d5aa0c78d9267a278ed768838b70cf5c72237a5c
Fatima-118 <Safoo.m23@icloud.com>
2026-05-20 17:55:49 +0300

    تعديلات على تاكيد الايميل

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/auth.controller');
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 router.post('/login', authController.login);
-
+// مسار إعادة إرسال الكود المرتبط بزر Sent Again
+router.post('/resend-otp', authController.resendOTP);
 module.exports = router;
